baf1ae585c3ffacd7a320bbc48453235122baf41
shufengwu <sfwandroid@163.com>
2018-10-20 15:27:10 +0800

    webpack base

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import "./index.css";
+let config = require('./config.json');
+window.onload = function(){
+    console.log(config.greetText);
+    //console.log(index_style);
+    
+    //document.getElementById("div1").classList.add("div1");
+}
